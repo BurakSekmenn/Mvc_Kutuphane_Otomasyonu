28539d013d1a83b923e8680687f31067696b76c7
Burak Sekmen <burakskmn07@gmail.com>
2023-09-26 01:43:00 +0300

    getall

---
+++ Mvc_Kutuphane_Otomasyonu/Controllers/KitapTurleriController.cs
@@ -0,0 +1,22 @@
+﻿using Mvc_Kutuphane_Otomasyonu.Entities.DAL;
+using Mvc_Kutuphane_Otomasyonu.Entities.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Kutuphane_Otomasyonu.Controllers
+{
+    public class KitapTurleriController : Controller
+    {
+        KutuphaneContext context = new KutuphaneContext();
+        KitapTurleriDAL kitapTurleriDAL = new KitapTurleriDAL();
+        // GET: KitapTurleri
+        public ActionResult Index()
+        {
+            var model = kitapTurleriDAL.GetAll(context);
+            return View(model);
+        }
+    }
+}
